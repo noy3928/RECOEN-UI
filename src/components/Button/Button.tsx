a171c5b071d363a930fc9804dac82e247722dd12
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-03-14 20:54:55 +0900

    fix : using component style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,21 +6,19 @@ export interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <S.Button>{props.text}</S.Button>
+  return <StyledButton>{props.text}</StyledButton>
 }
 
 export default Button
 
-const S = {
-  Button: styled.button`
-    padding: 32px;
-    background-color: hotpink;
-    font-size: 24px;
-    border-radius: 4px;
-    color: black;
-    font-weight: bold;
-    &:hover {
-      color: white;
-    }
-  `,
-}
+const StyledButton = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
