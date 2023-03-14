1fd167fc0b11db0d12c896d94de6f19c7a0240a3
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-03-14 20:52:14 +0900

    feat : add emomtionjs

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,26 @@
 import React from "react"
+import styled from "@emotion/styled"
 
 export interface ButtonProps {
   text: string
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.text}</button>
+  return <S.Button>{props.text}</S.Button>
 }
 
 export default Button
+
+const S = {
+  Button: styled.button`
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    color: black;
+    font-weight: bold;
+    &:hover {
+      color: white;
+    }
+  `,
+}
